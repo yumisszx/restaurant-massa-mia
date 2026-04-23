bd8fc1408bdadaafb84eb2e86e22eab0772f2585
yumi <ligiayumimeira@gmail.com>
2026-04-23 20:11:02 -0300

    feat: adiciona estrutura inicial index

---
+++ src/frontend/massamia/src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+
+const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <nav className="navbar">
+        <p className="name">Massa Mia</p>
+
+        <button
+          className="btn btn-mobile"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <i className={`fa-solid ${menuOpen ? "fa-times" : "fa-bars"}`}></i>
+        </button>
+
+        <ul id="nav-links" className={menuOpen ? "show" : ""}>
+          <li>
+            <a href="#produtos">Produtos</a>
+          </li>
+
+          <li>
+            <a href="">Sobre Nós</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
