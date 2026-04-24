be1067849dc301884b2b7db657e781e8706807b8
yumi <ligiayumimeira@gmail.com>
2026-04-24 09:47:29 -0300

    style: modifica variavel das cores

--- src/frontend/massamia/src/App.tsx
+++ src/frontend/massamia/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 
